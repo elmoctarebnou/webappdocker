cd685719dcec10781e8be5e13b6704724c42da2a
El Moctar Ebnou <ebnoumoctar@gmail.com>
2020-12-13 22:09:36 -0600

    webapp docker

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# Specifiy a base image
+FROM node:alpine
+
+WORKDIR /usr/app
+
+# Install some dependencies
+COPY ./package.json ./
+RUN npm install
+COPY ./ ./
+# Default command
+CMD ["npm", "start"]
+
+
---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hi there');
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
+});
+
